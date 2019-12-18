525508689ebe1a3bdd7db09b5341a43cd2c47acf
Olly Taylor <ollyjtaylor@live.co.uk>
2019-12-18 10:44:12 +0000

    added models, controllers and CRUD methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+  get 'posts/show'
+  get 'posts/new'
+  get 'posts/create'
+  get 'posts/edit'
+  get 'posts/destroy'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
+  get 'users/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
