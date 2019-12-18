8d5165806012af86adb92c3f6cbb17a861c2f73e
Olly Taylor <ollyjtaylor@live.co.uk>
2019-12-18 12:00:17 +0000

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,20 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/show'
-  get 'posts/new'
-  get 'posts/create'
-  get 'posts/edit'
-  get 'posts/destroy'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
-  get 'users/edit'
+
+  resources :users do
+    resources :posts
+  end
+
+  # get 'posts', to: 'posts#index'
+  # get 'posts/:id', to: 'posts#show', as: 'post'
+  # get 'posts/new', to: 'posts#new'
+  # post 'posts', to: 'posts#create'
+  # get 'posts/:id/edit', to: 'posts#edit'
+  # patch 'posts/:id', to: 'posts#update'
+  # delete 'posts/:id', to: 'posts#destroy'
+  # get 'users/:id', to: 'users#show', as: 'user'
+  # get 'users/new', to: 'users#new'
+  # post 'users', to: 'users#create'
+  # get 'users/:id/edit', to: 'posts#edit'
+  # patch 'users/:id', to: 'users#update'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
